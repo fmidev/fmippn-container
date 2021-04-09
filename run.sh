51b89d70c8ac6c05214073c0424dd0895ba68a82
RadAdm <radadm@nowcast1.fmi.fi>
2021-04-09 11:45:51 +0300

    New run script for nowcast servers

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DOMAIN=${DOMAIN:-'ravake_hdf5'}
+
+#Read short hostname from server to use as output folder names.
+HOSTNAME=`hostname -s`
+NODE=${NODE:-${HOSTNAME}}
+
+#List latest file in domain folder
+LATEST_TIMESTAMP=`ls -t /nowcast/data/input_composites/$DOMAIN/ | head -n1 | awk -F "_" '{print $1}'`
+
+TIMESTAMP=${TIMESTAMP:-${LATEST_TIMESTAMP}}
+
+echo "Calculating PPN nowcast for domain "$DOMAIN", timestamp "$TIMESTAMP
+
+# Build from Dockerfile
+#podman build --tag fmippn_pysteps14 -f Dockerfile_pysteps14
+
+# Run with volume mounts
+podman run \
+       --log-level=debug \
+       --rm \
+       --env "timestamp=$TIMESTAMP" \
+       --env "domain=$DOMAIN" \
+       --mount type=bind,source=/nowcast/data/input_composites/$DOMAIN,target=/input \
+       --mount type=bind,source=/nowcast/data/ppn_output/${DOMAIN}/$NODE,target=/output \
+       --mount type=bind,source=/nowcast/data/log/$NODE,target=/log \
+       --mount type=bind,source="$(pwd)"/${DOMAIN}_container.json,target=/fmippn-oper/fmippn/config/$DOMAIN.json \
+       --security-opt label=disable \
+       fmippn_pysteps14:latest
